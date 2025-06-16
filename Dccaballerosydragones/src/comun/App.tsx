cca5d862392924bbfb70bebd6e942fa57c0d0ebc
sjdonosoa <sjdonosoa@uc.cl>
2025-06-15 20:05:18 -0400

    Cambios para deploy 3

--- Dccaballerosydragones/src/comun/App.tsx
+++ Dccaballerosydragones/src/comun/App.tsx
@@ -1,13 +1,15 @@
+//import { useState } from 'react'
 import './App.css'
 import { useAuth } from './AuthContext'; // Importa el contexto de autenticación
 import Navbar_usuario from './componentes/Navbar_usuario';
 import Navbar from './componentes/Navbar'
 import Dragon from './componentes/Dragon'
 import Caballero from './componentes/Caballero'
-
+import Ranking from '../game/ranking';
+import FinalPartida from '../game/final_partida';
 
 function App() {
-
+  //const [count, setCount] = useState(0)
   const { isAuthenticated } = useAuth();
 
   return (
@@ -20,6 +22,8 @@ function App() {
       </div>
       <Dragon />
       <Caballero />
+      <Ranking />
+      <FinalPartida />
     </>
   )
 }
